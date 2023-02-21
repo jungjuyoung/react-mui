9d34dbf9bfb7c3bb558ffea089d30b33932757a0
jungjuyoung <nwwn1004@naver.com>
2023-02-21 23:08:17 +0900

    edited font weight

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,15 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
 const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Box position="fixed">Rightbar</Box>
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+      </Box>
     </Box>
   );
 };

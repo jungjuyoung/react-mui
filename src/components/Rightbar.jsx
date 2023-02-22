a4f0b0af8fc11ee5c0eddd316df3ebb1a267b77e
jungjuyoung <nwwn1004@naver.com>
2023-02-23 00:25:42 +0900

    avatar image connect

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -10,11 +10,26 @@ const Rightbar = () => {
           Online Friends
         </Typography>
         <AvatarGroup total={7}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://material-ui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://material-ui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/5.jpg"
+          />
         </AvatarGroup>
+        <Typography variant="h6" fontWeight={100}>
+          Latest Photos
+        </Typography>
       </Box>
     </Box>
   );

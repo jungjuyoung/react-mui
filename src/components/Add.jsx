52794471f5bdfd89aedbef0f5df5ef14c027fbd2
jungjuyoung <nwwn1004@naver.com>
2023-02-24 00:10:04 +0900

    adding modal ui complete

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import { Fab, Tooltip } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+
+import TextField from "@mui/material/TextField";
 import AddIcon from "@mui/icons-material/Add";
+import { Box, styled } from "@mui/system";
+import Stack from "@mui/material/Stack";
+import {
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 
 function Add() {
+  const [open, setOpen] = useState(false);
   return (
     <React.Fragment>
       <Tooltip
         title="Delete"
+        onClick={e => setOpen(true)}
         sx={{
           position: "fixed",
           bottom: 20,
@@ -20,6 +54,51 @@ function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={e => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://material-ui.com/static/images/avatar/1.jpg"
+            />
+            <Typography variant="span" fontWeight={500}>
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={4}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </React.Fragment>
   );
 }
